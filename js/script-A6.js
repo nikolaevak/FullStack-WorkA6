d430b3181fc66f41cd2ac8a6c229737503232fdf
nikolaevak <nikolaevak@gmail.com>
2020-09-27 23:14:18 +0300

    Add files via upload

---
+++ js/script-A6.js
@@ -0,0 +1,44 @@
+﻿
+
+$(document).ready(function() {
+var progbar  = $(".progress-bar"),
+max = progbar.attr('aria-valuemax'),
+prg = 0;
+var msg = 100 + '% Complete';
+console.log ("скрипт прогрузился", max, prg)
+
+//при нажатии на 1 Кнопку 
+$(".btn-start-1").click(function(){
+    prg += 1 
+    $('.progress-bar').css('width', prg + '%').attr('aria-valuenow', prg);
+    if (prg >= max) {
+        $('.progress-bar').text(msg);
+      }
+   
+ });
+
+ //при нажатии на кнопку 2 
+
+  $(".btn-start-2").click(function(){
+    prg += 3
+    $('.progress-bar').css('width', prg + '%').attr('aria-valuenow', prg);
+    if (prg >= max) {
+        $('.progress-bar').text(msg);
+      }
+  });
+
+  //при нажатии на кнопку 3
+
+  $(".btn-start-3").click(function(){
+    prg += 7
+    $('.progress-bar').css('width', prg + '%').attr('aria-valuenow', prg);
+    if (prg >= max) {
+        $('.progress-bar').text(msg);
+      }
+  });
+
+ 
+
+})
+
+
